f57ee98e1963be46bf0a284876cb8ca7eca95e6c
Sam Goldman <sam@samgoldman.net>
2019-09-19 20:51:06 -0400

    adding more logging about temp

--- run.rb
+++ run.rb
@@ -12,13 +12,15 @@ max_temp = target_temp + (Config::Temp::RANGE / 2)
 inside_thermometer = TempReader.new(Config::Temp::Devices::INSIDE)
 outside_thermometer = TempReader.new(Config::Temp::Devices::OUTSIDE)
 outside_temp = outside_thermometer.read
+inside_temp = inside_thermometer.read
+
+puts "inside: #{inside_temp}  |  outside: #{outside_temp}"
 
 if outside_temp < min_temp || outside_temp > max_temp
   puts 'Outside temperature is not within pleasant range.'
   exit 0
 end
 
-inside_temp = inside_thermometer.read
 if (target_temp - outside_temp).abs > (target_temp - inside_temp).abs
   puts 'Outside temperature is less pleasant than inside.'
   exit 0
